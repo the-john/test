88f0a32fbc00ebb199bc61312178b16b455f0787
root <delton.oliver@outlook.com>
2019-12-03 17:46:39 +0000

    first commit

---
+++ build/CMakeFiles/capstone.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/capstone.dir/src/main.cpp.o"
+  "capstone.pdb"
+  "capstone"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/capstone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include </usr/local/include/opencv4/opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+    cv::Mat image = cv::imread("/home/workspace/capstone/data/John_Oliver_PIC.jpg");
+
+    return 0;
+}
